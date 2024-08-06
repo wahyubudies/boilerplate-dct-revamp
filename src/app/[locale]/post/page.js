d72029a58db9588745d4f05434ccac49401c675a
wahyubudies.dct <wahyu.setyawan@digitalcenter.id>
2024-08-06 15:05:30 +0700

    ini contoh promise all

--- src/app/[locale]/post/page.js
+++ src/app/[locale]/post/page.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Table, Filter } from "@/components/post";
 
-async function getData() {
+const getPage2 = async () => {
   const res = await fetch("https://reqres.in/api/users?page=2", {
     method: "GET",
   });
-  console.log(res);
-
   return res.json();
+};
+
+const getPage1 = async () => {
+  const res = await fetch("https://reqres.in/api/users?page=1", {
+    method: "GET",
+  });
+  return res.json();
+};
+
+async function getData() {
+  const [res1, res2] = await Promise.all([getPage1(), getPage2()]);
+
+  return [
+    ...res1.data,
+    ...res2.data,
+  ];
 }
 
 export default async function Page() {
-  const { data } = await getData();
-  console.log(data);
+  const data = await getData();
+  
+  // console.log("===================================");
+  // console.log("🚀 ~ Page ~ data:", data);
+  // return null;
 
   return (
     <div>
